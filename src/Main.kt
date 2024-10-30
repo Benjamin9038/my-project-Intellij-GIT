24afc9cb5333821864430764397e296ad1a6f445
benjamin <bpa415424@iesmorcillo.es>
2024-10-30 12:24:16 +0100

    Creation and using fun helloworld

--- src/Main.kt
+++ src/Main.kt
@@ -2,4 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main(args:Array<String>) {
    println("Esta es la prueba de la conexion github/intellij")
+    helloworld("Benjamin Alumno")
     }
+fun helloworld(name: String){
+    println("hello $name")
+}
